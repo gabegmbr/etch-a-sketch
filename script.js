612690994e59c17cd16ebf79961db2552c949f9a
gabegmbr <221022560@aluno.unb.br>
2024-07-06 15:35:25 -0300

    grid and grid size slider implemented

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-const gridContainer = document.getElementById("grid");
+let gridContainer = document.getElementById("grid");
+let slider = document.getElementById("myRange");
+let gridText = document.getElementById("sizeText")
 
-createGrid(16);
+gridText.textContent = "Grid size: 5x5";
+createGrid(5);
+
+slider.oninput = function() {
+    document.querySelectorAll('.gridBlocks').forEach(e => e.remove());
+    document.querySelectorAll('.gridRows').forEach(e => e.remove());
+    createGrid(slider.value);
+    gridText.textContent = `Grid size: ${slider.value}x${slider.value}`
+}
+
+let color = "rgb(0, 0, 0)";
+
+
 
 function createGrid(x){
     for(let i = 1; i <= x; i++){
-        const gridRow = document.createElement("div");
+        let gridRow = document.createElement("div");
+        gridRow.classList.add("gridRows");
         for(let y = 1; y <= x; y++){
-            const gridItem = document.createElement("div");
-            gridItem.textContent = i;
-            gridItem.style.color = "white";
+            let gridItem = document.createElement("div");
+            gridItem.classList.add("gridBlocks");
+            gridItem.addEventListener("mouseover", () => {
+                gridItem.style.backgroundColor = color;
+            });
             gridRow.append(gridItem);
         }
         gridContainer.append(gridRow);
